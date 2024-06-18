8a38c3d23c48bb7be8ae81cb1db7b7e8dc667df2
eclipse2ant <eclipse2ant@gmail.com>
2024-06-18 17:15:12 +0900

    `users can create new lists`

--- src/test/kotlin/ddt/com/ubertob/fotf/zettai/stories/SeeAllTheToDoListsDDT.kt
+++ src/test/kotlin/ddt/com/ubertob/fotf/zettai/stories/SeeAllTheToDoListsDDT.kt
@@ -9,6 +9,7 @@ import ddt.com.ubertob.fotf.zettai.tooling.allActions
 class SeeAllTheToDoListsDDT: ZettaiDDT(allActions()) {
     val carol by NamedActor(::ToDoListOwner)
     val emma by NamedActor(::ToDoListOwner)
+    val dylan by NamedActor(::ToDoListOwner)
 
     @DDT
     fun `no users have no lists`() = ddtScenario {
@@ -23,6 +24,15 @@ class SeeAllTheToDoListsDDT: ZettaiDDT(allActions()) {
         }.thenPlay(
             carol.`can see the lists #listNames`(expectedLists.keys),
             emma.`cannot see any list`())
+    }
+    @DDT
+    fun `users can create new lists`() = ddtScenario {
+        play(
+            dylan.`cannot see any list`(),
+            dylan.`can create a new list called #listname`("gardening"),
+            dylan.`can create a new list called #listname`("music"),
+            dylan.`can see the lists #listNames`(setOf("gardening", "music"))
+        ).wip(LocalDate.of(2023,12,31), "working on it!")
     }
 
     private fun generateSomeToDoLists(): Map<String, List<String>> {
