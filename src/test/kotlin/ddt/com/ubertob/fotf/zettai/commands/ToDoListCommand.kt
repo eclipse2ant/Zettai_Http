87d2d37b33a50435e49061aa395494f458394cd6
eclipse2ant <eclipse2ant@gmail.com>
2024-06-24 14:09:36 +0900

    ToDoListCommand

---
+++ src/test/kotlin/ddt/com/ubertob/fotf/zettai/commands/ToDoListCommand.kt
@@ -0,0 +1,12 @@
+package ddt.com.ubertob.fotf.zettai.commands
+
+import com.ubertob.fotf.zettai.domain.ListName
+import com.ubertob.fotf.zettai.domain.ToDoItem
+import com.ubertob.fotf.zettai.domain.User
+
+sealed class ToDoListCommand
+
+data class CreateToDoList(val user: User, val name: ListName) : ToDoListCommand() {
+    val id: ToDoListId = ToDoListId.mint()
+}
+data class AddToDoItem(val user: User, val name: ListName, val item: ToDoItem) : ToDoListCommand()
