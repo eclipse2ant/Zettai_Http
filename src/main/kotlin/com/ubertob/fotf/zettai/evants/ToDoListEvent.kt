32c151c7e76c40ca9329fa5f80139852f2be4784
eclipse2ant <eclipse2ant@gmail.com>
2024-07-08 20:38:44 +0900

    ActiveToDoList  & InitialState

--- src/main/kotlin/com/ubertob/fotf/zettai/evants/ToDoListEvent.kt
+++ src/main/kotlin/com/ubertob/fotf/zettai/evants/ToDoListEvent.kt
@@ -6,6 +6,7 @@ import com.ubertob.fotf.zettai.domain.User
 import com.ubertob.fotf.zettai.fp.EntityEvent
 import com.ubertob.fotf.zettai.fp.EntityState
 import com.ubertob.fotf.zettai.fp.ToDoListId
+import jdk.internal.jshell.debug.InternalDebugControl.release
 import java.time.Instant
 
 sealed class ToDoListEvent: EntityEvent
@@ -32,11 +33,64 @@ data class ListClosed(
     val closedOn: Instant
 ): ToDoListEvent()
 
+fun Iterable<ToDoListEvent>.fold(): ToDoListState =
+    fold(InitialState as ToDoListState) { acc, e -> acc.combine(e) }
+
+
 sealed class ToDoListState : EntityState<ToDoListEvent> {
     abstract override fun combine(event: ToDoListEvent): ToDoListState
 }
-object InitialState: ToDoListState() {
-    override fun combine(event: ToDoListEvent) = this // for the moment
+object InitialState : ToDoListState() {
+    override fun combine(event: ToDoListEvent): ToDoListState =
+        when (event) {
+            is ListCreated -> create(event.id, event.owner, event.name, emptyList())
+            else -> this //ignore other events
+        }
 }
-fun Iterable<ToDoListEvent>.fold(): ToDoListState =
-    fold(InitialState as ToDoListState) { acc, e -> acc.combine(e) }
+
+data class ActiveToDoList internal constructor(
+    val id: ToDoListId,
+    val owner: User,
+    val name: ListName,
+    val items: List<ToDoItem>
+) :
+    ToDoListState() {
+    override fun combine(event: ToDoListEvent): ToDoListState =
+        when (event) {
+            is ItemAdded -> copy(items = items + event.item)
+            is ItemRemoved -> copy(items = items - event.item)
+            is ItemModified -> copy(items = items - event.prevItem + event.item)
+            is ListPutOnHold -> onHold(event.reason)
+            is ListClosed -> close(event.closedOn)
+            else -> this //ignore other events
+        }
+}
+
+data class OnHoldToDoList internal constructor(
+    val id: ToDoListId,
+    val owner: User,
+    val name: ListName,
+    val items: List<ToDoItem>,
+    val reason: String
+) :
+    ToDoListState() {
+    override fun combine(event: ToDoListEvent): ToDoListState =
+        when (event) {
+            is ListReleased -> release()
+            else -> this //ignore other events
+        }
+}
+
+data class ClosedToDoList internal constructor(val id: ToDoListId, val closedOn: Instant) : ToDoListState() {
+    override fun combine(event: ToDoListEvent): ToDoListState = this //ignore other events
+
+}
+
+fun InitialState.create(id: ToDoListId, owner: User, name: ListName, items: List<ToDoItem>) =
+    ActiveToDoList(id, owner, name, items)
+
+fun ActiveToDoList.onHold(reason: String) = OnHoldToDoList(id, owner, name, items, reason)
+
+fun OnHoldToDoList.release() = ActiveToDoList(id, owner, name, items)
+
+fun ActiveToDoList.close(closedOn: Instant) = ClosedToDoList(id, closedOn)
