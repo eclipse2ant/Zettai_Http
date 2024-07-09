0439a52d9d961b6fd5347ed4ad4a067760a6ea89
eclipse2ant <eclipse2ant@gmail.com>
2024-07-09 16:13:30 +0900

    ToDoListCommandsTest

---
+++ src/test/kotlin/com/ubertob/fotf/zettai/commands/ToDoListCommandsTest.kt
@@ -0,0 +1,25 @@
+package com.ubertob.fotf.zettai.commands
+
+import com.ubertob.fotf.zettai.domain.ListName
+import com.ubertob.fotf.zettai.domain.User
+import com.ubertob.fotf.zettai.domain.randomListName
+import com.ubertob.fotf.zettai.domain.randomUser
+import com.ubertob.fotf.zettai.evants.InitialState
+import com.ubertob.fotf.zettai.evants.ListCreated
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+
+internal class ToDoListCommandsTest {
+    val fakeRetriever: ToDoListRetriever = {
+            (user: User, listName: ListName) -> InitialState
+    }
+    @Test
+    fun `CreateToDoList generate the correct event`() {
+        val cmd = CreateToDoList(randomUser(), randomListName())
+        val handler = ToDoListCommandHandler(fakeRetriever)
+        val res = handler(cmd)?.single()
+        expectThat(res).isEqualTo(
+            ListCreated(cmd.id, cmd.user, cmd.name)
+        )
+    }
+}
