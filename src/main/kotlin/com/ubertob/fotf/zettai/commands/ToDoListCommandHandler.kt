959d553f8fd4e5fad1b74fa171d8dc9abf916a53
eclipse2ant <eclipse2ant@gmail.com>
2024-07-09 20:46:05 +0900

    ToDoListCommandHandler 2

--- src/main/kotlin/com/ubertob/fotf/zettai/commands/ToDoListCommandHandler.kt
+++ src/main/kotlin/com/ubertob/fotf/zettai/commands/ToDoListCommandHandler.kt
@@ -2,14 +2,29 @@ package com.ubertob.fotf.zettai.commands
 
 import com.ubertob.fotf.zettai.domain.ListName
 import com.ubertob.fotf.zettai.domain.User
+import com.ubertob.fotf.zettai.evants.InitialState
+import com.ubertob.fotf.zettai.evants.ListCreated
 import com.ubertob.fotf.zettai.evants.ToDoListEvent
 import com.ubertob.fotf.zettai.evants.ToDoListState
 
 
-typealias CommandHandler<CMD, EVENT> = (CMD) -> List<EVENT>?
-class ToDoListCommandHandler:
-    CommandHandler<ToDoListCommand, ToDoListEvent> {
-    override fun invoke(command: ToDoListCommand) = TODO()
+class ToDoListCommandHandler(val entityRetriever: ToDoListRetriever
+): (ToDoListCommand) -> List<ToDoListEvent>? {
+    override fun invoke(command: ToDoListCommand) : List<ToDoListEvent>? =
+        when (command) {
+            is CreateToDoList -> command.execute()
+            else -> null //ignore for the moment
+        }
+    private fun CreateToDoList.execute(): List<ToDoListEvent>? =
+        entityRetriever.retrieveByName(user, name)
+            ?.let { listState ->
+                when (listState) {
+                    InitialState -> {
+                        ListCreated(id, user, name).toList()
+                    }
+                    else -> null //command fail
+                }
+            }
 }
 
 typealias ToDoListRetriever =
