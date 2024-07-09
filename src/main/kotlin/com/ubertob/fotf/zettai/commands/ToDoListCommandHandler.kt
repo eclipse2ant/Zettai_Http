b5221ea2ee1780ef09144179d23df5d41eaed52a
eclipse2ant <eclipse2ant@gmail.com>
2024-07-09 15:49:40 +0900

    typealias ToDoListRetriever

--- src/main/kotlin/com/ubertob/fotf/zettai/commands/ToDoListCommandHandler.kt
+++ src/main/kotlin/com/ubertob/fotf/zettai/commands/ToDoListCommandHandler.kt
@@ -1,6 +1,9 @@
 package com.ubertob.fotf.zettai.commands
 
+import com.ubertob.fotf.zettai.domain.ListName
+import com.ubertob.fotf.zettai.domain.User
 import com.ubertob.fotf.zettai.evants.ToDoListEvent
+import com.ubertob.fotf.zettai.evants.ToDoListState
 
 
 typealias CommandHandler<CMD, EVENT> = (CMD) -> List<EVENT>?
@@ -8,3 +11,6 @@ class ToDoListCommandHandler:
     CommandHandler<ToDoListCommand, ToDoListEvent> {
     override fun invoke(command: ToDoListCommand) = TODO()
 }
+
+typealias ToDoListRetriever =
+            (user: User, listName: ListName) -> ToDoListState?
