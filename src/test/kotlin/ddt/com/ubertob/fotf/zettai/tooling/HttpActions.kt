59ed2bd5de5419cfd19f3dc3878303ce50283a71
eclipse2ant <eclipse2ant@gmail.com>
2024-08-01 16:08:11 +0900

    Rewritting HttpActioons

--- src/test/kotlin/ddt/com/ubertob/fotf/zettai/tooling/HttpActions.kt
+++ src/test/kotlin/ddt/com/ubertob/fotf/zettai/tooling/HttpActions.kt
@@ -1,11 +1,16 @@
 package ddt.com.ubertob.fotf.zettai.tooling
 
-import com.ubertob.pesticide.core.*
+import com.ubertob.fotf.zettai.commands.AddToDoItem
+import com.ubertob.fotf.zettai.commands.CreateToDoList
 import com.ubertob.fotf.zettai.domain.*
 import com.ubertob.fotf.zettai.ui.HtmlPage
 import com.ubertob.fotf.zettai.ui.toIsoLocalDate
 import com.ubertob.fotf.zettai.ui.toStatus
 import com.ubertob.fotf.zettai.webserver.Zettai
+import com.ubertob.pesticide.core.DdtProtocol
+import com.ubertob.pesticide.core.DomainSetUp
+import com.ubertob.pesticide.core.Http
+import com.ubertob.pesticide.core.Ready
 import org.http4k.client.JettyClient
 import org.http4k.core.Method
 import org.http4k.core.Request
@@ -17,28 +22,30 @@ import org.http4k.server.Jetty
 import org.http4k.server.asServer
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.junit.jupiter.api.fail
 import strikt.api.expectThat
+import strikt.assertions.hasSize
 import strikt.assertions.isEqualTo
 
 
 data class HttpActions(val env: String = "local") : ZettaiActions {
 
-    private val fetcher = ToDoListFetcherFromMap(mutableMapOf())
-    private val hub = ToDoListHub(fetcher)
-
     val zettaiPort = 8000 //different from the one in main
-    val server = Zettai(hub).asServer(Jetty(zettaiPort))
-
-    override val protocol: DdtProtocol = Http(env)
+    val zettai = prepareZettaiForTests()
+    val server = zettai.asServer(Jetty(zettaiPort))
+    val hub = zettai.hub
 
     val client = JettyClient()
 
+    override val protocol: DdtProtocol = Http(env)
     override fun prepare(): DomainSetUp {
         server.start()
         return Ready
     }
+
     override fun tearDown(): HttpActions =
         also { server.stop() }
+
     override fun getToDoList(user: User, listName: ListName): ToDoList? {
 
         val response = callZettai(Method.GET, todoListUrl(user, listName))
@@ -54,12 +61,6 @@ data class HttpActions(val env: String = "local") : ZettaiActions {
 
         return ToDoList(listName, items)
     }
-    override fun ToDoListOwner.`starts with a list`(listName: String, items: List<String>) {
-        fetcher.assignListToUser(
-            user,
-            ToDoList(ListName.fromUntrustedOrThrow(listName), items.map { ToDoItem(it) })
-        )
-    }
 
     override fun addListItem(user: User,
                              listName: ListName, item: ToDoItem) {
@@ -70,6 +71,15 @@ data class HttpActions(val env: String = "local") : ZettaiActions {
         )
         expectThat(response.status).isEqualTo(Status.SEE_OTHER)
     }
+
+    override fun ToDoListOwner.`starts with a list`(listName: String, items: List<String>) {
+        fetcher.assignListToUser(
+            user,
+            ToDoList(ListName.fromUntrustedOrThrow(listName), items.map { ToDoItem(it) })
+        )
+    }
+
+
 
     override fun allUserLists(user: User): List<ListName> {
         val response = callZettai(Method.GET, allUserListsUrl(user))
